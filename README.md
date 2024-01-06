aa603c0449e488922916a29e485f22780949aa28
agrahjain786 <agrahjain02@gmail.com>
2024-01-07 02:48:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# autoreplysystem
+ Node.js based app that is able to respond to emails sent to your Gmail mailbox while you’re out on a vacation. 
---
+++ credentials.js
@@ -0,0 +1,5 @@
+const CLIENT_ID = "284907945869-opg20ak2a9hepue8kd3gmkgq045pnlq9.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN ="1//04fIAD52Xjy0SCgYIARAAGAQSNwF-L9IrmCJoTz7ai6o1kT2H3vaCr1sRu0tFNZKVkn9FveGXelGcvbSgQtNVYHI8EGSwa_cIwts";
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
