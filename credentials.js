361c7509e9f8b7631892609892829aeb7d5ea7d3
agrahjain786 <131575356+agrahjain786@users.noreply.github.com>
2024-01-09 23:54:21 +0530

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = "284907945869-opg20ak2a9hepue8kd3gmkgq045pnlq9.apps.googleusercontent.com";
+const CLIENT_ID = "";
 const CLEINT_SECRET = "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";
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
 const REFRESH_TOKEN ="";
